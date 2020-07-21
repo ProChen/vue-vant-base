974f829ea7dc8f42b5795d1c8461a2394e558a69
ChenLei <1224532706@qq.com>
2020-07-21 14:54:21 +0800

    feat: 增加了一个新的功能

--- src/main.js
+++ src/main.js
@@ -29,8 +29,8 @@ Vue.use(Navigation, {
 
 // 开发环境下面使用vConsole进行调试
 if (process.env.NODE_ENV === 'development') {
-  const VConsole = require('vconsole')
-  new VConsole()
+  // const VConsole = require('vconsole')
+  // new VConsole()
 }
 
 new Vue({
